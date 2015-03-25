e3af8150ce1d0cc52035ea8ca8fadad7ada9324d
Evans55 <jonathanccoicca@gmail.com>
2015-03-25 18:19:45 -0500

    cambiosss

--- JS/testing_PG.js
+++ JS/testing_PG.js
@@ -11,6 +11,10 @@ $("#resultado").html("PhoneGap está en marcha");
 
 var watch=navigator.accelerometer.watchAcceleration(onInfo,onError,{frequency:2000});
 
+//navigator.accelerometer.clearWatch(watch);
+
+var media = new Media(src, recordAudio);
+
 }
 
 
@@ -25,3 +29,23 @@ $("#resultado")	.html(' Acceleration X: ' + acceleration.x + '<br>' +
 function onError() {
     alert('No hay acelerómetro');
 };
+
+
+// Record audio
+//
+function recordAudio() {
+    var src = "Ellie Goulding-Outside.mp3";
+    var mediaRec = new Media(src,
+        // success callback
+        function() {
+            console.log("recordAudio():Audio Success");
+        },
+
+        // error callback
+        function(err) {
+            console.log("recordAudio():Audio Error: "+ err.code);
+        });
+
+    // Record audio
+    mediaRec.startRecord();
+}
