b27cedbc2f94ded9e5f38256b357e52989872c9d
Evans55 <jonathanccoicca@gmail.com>
2015-03-25 17:48:58 -0500

    cambioss

--- JS/testing_PG.js
+++ JS/testing_PG.js
@@ -6,4 +6,19 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady(){
 $("#resultado").html("PhoneGap está en marcha");	
+
+navigator.accelerometer.getCurrentAcceleration(onInfo,onError);
 }
+
+
+function onInfo(acceleration){
+$("#resultado")	.append('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+	
+}
+
+function onError() {
+    alert('No hay acelerómetro');
+};
