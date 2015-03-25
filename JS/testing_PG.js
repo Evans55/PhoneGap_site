fc5ee5f492d9201737ce010c25b838c7458b46c7
Evans55 <jonathanccoicca@gmail.com>
2015-03-25 18:02:23 -0500

    cambi

--- JS/testing_PG.js
+++ JS/testing_PG.js
@@ -7,15 +7,18 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady(){
 $("#resultado").html("PhoneGap está en marcha");	
 
-navigator.accelerometer.getCurrentAcceleration(onInfo,onError);
+//navigator.accelerometer.getCurrentAcceleration(onInfo,onError);
+
+var watch=navigator.accelerometer.watchAcceleration(onInfo,onError,{frequency:2000});
+
 }
 
 
 function onInfo(acceleration){
-$("#resultado")	.append('Acceleration X: ' + acceleration.x + '\n' +
-          'Acceleration Y: ' + acceleration.y + '\n' +
-          'Acceleration Z: ' + acceleration.z + '\n' +
-          'Timestamp: '      + acceleration.timestamp + '\n');
+$("#resultado")	.html(' Acceleration X: ' + acceleration.x + '<br>' +
+          'Acceleration Y: ' + acceleration.y + '<br>' +
+          'Acceleration Z: ' + acceleration.z + '<br>' +
+          'Timestamp: '      + acceleration.timestamp + '<br>');
 	
 }
 
